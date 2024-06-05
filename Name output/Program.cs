81b51edbcbe51be12c8e8e4ecc7028db5bc04aea
AleksandrGHub <bulanov.shura@gmail.com>
2024-06-05 21:01:55 +0300

    Добавьте файлы проекта.

---
+++ Name output/Program.cs
@@ -0,0 +1,38 @@
+﻿namespace Name_output
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string name;
+            string symbol;
+            int lengthName;
+            int lengthString;
+            int positionCursorX = 1;
+            int positionCursorY = 1;
+            int quantityStrings = 3;
+            int quantityAdditionalCharacters = 2;
+
+            name = Console.ReadLine();
+            symbol = Console.ReadLine();
+            lengthName = name.Length;
+            lengthString = lengthName + quantityAdditionalCharacters;
+            Console.Clear();
+
+            for (int i = 0; i < quantityStrings; i++)
+            {
+                for (int y = 0; y < lengthString; y++)
+                {
+                    Console.Write(symbol);
+                }
+
+                Console.Write("\n");
+            }
+
+            Console.SetCursorPosition(positionCursorX, positionCursorY);
+            Console.Write(name);
+            Console.Write("\n");
+            Console.Write("\n");
+        }
+    }
+}
